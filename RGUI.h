65ce18756593177dfa87a60abed89c52395a30de
Simon Urbanek <simon.urbanek@R-project.org>
2008-09-30 14:43:11 +0000

    bump up version to 1.26-dev
    git-svn-id: https://svn.r-project.org/R-packages/trunk/Mac-GUI@5236 694ef91d-65df-0310-b7bb-92e67a308ead

--- RGUI.h
+++ RGUI.h
@@ -34,7 +34,7 @@
 #define __RGUI_H__
 
 /* GUI version */
-#define R_GUI_VERSION_STR "1.25" /* it must be a string, but should parse to a valid real number */
+#define R_GUI_VERSION_STR "1.26" /* it must be a string, but should parse to a valid real number */
 
 /* macros for translatable strings */
 #define NLS(S) NSLocalizedString(S,@"")
