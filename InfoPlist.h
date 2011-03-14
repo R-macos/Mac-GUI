6a39859df1853b2d691587de4fa9d20a1f11d611
Simon Urbanek <simon.urbanek@R-project.org>
2011-03-14 16:32:48 +0000

    update credits and GUI version
    git-svn-id: https://svn.r-project.org/R-packages/trunk/Mac-GUI@5706 694ef91d-65df-0310-b7bb-92e67a308ead

--- InfoPlist.h
+++ InfoPlist.h
@@ -8,9 +8,9 @@
  */
 
 /* GUI version as shown in infos e.g. 1.27-devel */
-#define GUI_VER 1.36
+#define GUI_VER 1.40-devel
 /* R postfix used to denote release versions of GUI - set to R release version (e.g. 2.8.0) or to anything that will be shown in between R and GUI (e.g. - or for Mac) */
-#define R_RELEASE 2.12.2
+#define R_RELEASE for Mac OS X
 
 #if LEOPARD_CFG
 #define CFG_NAME Leopard build 32-bit
