5dfdbc6ca8a326179c584ef5183760463e3a6a62
Simon Urbanek <simon.urbanek@R-project.org>
2017-12-07 02:41:45 +0000

    R 3.4.3
    git-svn-id: https://svn.r-project.org/R-packages/trunk/Mac-GUI@7464 694ef91d-65df-0310-b7bb-92e67a308ead

--- InfoPlist.h
+++ InfoPlist.h
@@ -10,7 +10,7 @@
 /* GUI version as shown in infos e.g. 1.27-devel */
 #define GUI_VER 1.70
 /* R postfix used to denote release versions of GUI - set to R release version (e.g. 2.8.0) or to anything that will be shown in between R and GUI (e.g. - or for Mac) */
-#define R_RELEASE 3.4.2
+#define R_RELEASE 3.4.3
 
 #if RELEASE_CFG
 /* for release config just auto-detect the SDK used */
