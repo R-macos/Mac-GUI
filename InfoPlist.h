137a39337732fe1675046ac1dd0692d7ce0091a7
Simon Urbanek <simon.urbanek@R-project.org>
2020-06-07 09:36:07 +0000

    GUI 1.72
    git-svn-id: https://svn.r-project.org/R-packages/trunk/Mac-GUI@7845 694ef91d-65df-0310-b7bb-92e67a308ead

--- InfoPlist.h
+++ InfoPlist.h
@@ -8,9 +8,9 @@
  */
 
 /* GUI version as shown in infos e.g. 1.27-devel */
-#define GUI_VER 1.72-devel
+#define GUI_VER 1.72
 /* R postfix used to denote release versions of GUI - set to R release version (e.g. 2.8.0) or to anything that will be shown in between R and GUI (e.g. - or for Mac) */
-#define R_RELEASE for macOS
+#define R_RELEASE for R 4.0.1
 
 #if RELEASE_CFG
 /* for release config just auto-detect the SDK used */
